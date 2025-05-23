4578512a52ff088a9395574ede0d99b91fdd174d
jpereirasllc <contatojeoassilva@gmail.com>
2025-05-22 21:33:02 -0400

    Create inicializaProcesso.java

---
+++ src/inicializaProcesso.java
@@ -0,0 +1,14 @@
+/*
+
+	22/maio/2025 | 21:03
+*/
+
+package criptografiaSimetrica;
+
+public class InicializaProcesso {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+	}
+}
